16920679844be29b25cd8992613a9e212f3f1793
Hle14 <HansLe518@gmail.com>
2013-10-24 19:41:43 -0400

    cleaning up print statements
    print-statement debugging is messy

--- CppRigidBodyPFP/src/RigidBodyPlanner.cpp
+++ CppRigidBodyPFP/src/RigidBodyPlanner.cpp
@@ -25,13 +25,7 @@ RigidBodyMove RigidBodyPlanner::ConfigurationMove(void)
 {
 	RigidBodyMove move;
 	
-	//printf("Hello");
-	//add your implementation
-	
-	//calculate the U(q) potential
-	
 	int verts = m_simulator->GetNrRobotVertices();
-	//printf("%d",verts);
 	const double* vertices = m_simulator->GetRobotVertices();
 	double theta = m_simulator->GetRobotTheta();
 	double x = m_simulator->GetRobotX();
@@ -45,15 +39,13 @@ RigidBodyMove RigidBodyPlanner::ConfigurationMove(void)
 	double scale_rep = 3;
 	double threshold = 0.5; //arbitrarily chosen threshold distance for Urep
 	srand(time(NULL)); //seed random number generator to spike uRep after sufficient amount of time
-	double spike = 20.0; //amplifies uRep at random while near obstacles
+	double spike = 10.0; //amplifies uRep at random while near obstacles
 	int chance = 10; //changes probability of "spiking" uRep
 
 	//initialize default values for gradients
 	double uAtt[2] = {0,0};
 	double uRep[2] = {0,0};
 	double uCS[3] = {0,0,0};
-
-	//printf("uAtt.x = %f, uAtt.y = %f\nuRep.x = %f, uRep.y = %f",uAtt[0],uAtt[1],uRep[0],uRep[1]);
 
 	double Jacobian[2][3];
 	//set columns 0 and 1 on all Jacobians
@@ -67,7 +59,7 @@ RigidBodyMove RigidBodyPlanner::ConfigurationMove(void)
 		Jacobian[1][2] = 0;
 	}
 	Point oi;
-	//wait(); //HERE IS THE KEY THE KEY THE KEY THE KEY
+
 	for(int i=0;i<verts;i++)
 	{
 		//calc U.att(q)
@@ -83,18 +75,18 @@ RigidBodyMove RigidBodyPlanner::ConfigurationMove(void)
 			double ydist = oi.m_y - vertices[2*i+1];
 
 			double obs_dist = sqrt(pow(xdist,2) + pow(ydist,2));
-			//printf("\nobs_dist = %f",obs_dist);
 			
 			if(obs_dist < threshold) //if the obstacle is close enough, calc a repelling force for it
 			{
 				//set scale_rep based on distance
-				scale_rep = 2/pow(obs_dist,2);
-
+				scale_rep = 1/pow(obs_dist,2); //higher power of denominator increases scaling effect
+				//set scale_cs when approaching obstacle, decreasing the robot's velocity
 				scale_cs = .025*pow(obs_dist,2);
 
 				uRep[0] += xdist*scale_rep;
 				uRep[1] += ydist*scale_rep;
 
+				//while near obstacle, 1-in-chance chance to add large repelling force
 				if(rand()%chance==0) //add orthogonal vector with large magnitude to uRep
 				{
 					double temp = uRep[0];
@@ -109,33 +101,18 @@ RigidBodyMove RigidBodyPlanner::ConfigurationMove(void)
 					uRep[0] += orth_x;
 					uRep[1] += orth_y;
 				}
-
-				//spike = 1; //reset spike value
-
-				printf("\nspike = %f",spike);
-			}
-			//double disty = uRep[1]/scale_rep;
-			//printf("\ndistx = %f\ndisty = %f",distx,disty);
+			}			
 		}
-
-		spike = 1; //reset spike value
-
-		//printf("\nuAtt.x = %f, uAtt.y = %f\nuRep.x = %f, uRep.y = %f\noi.x = %f, oi.y = %f",uAtt[0],uAtt[1],uRep[0],uRep[1],oi.m_x,oi.m_y);
-		//wait(); //HERE IS THE KEY HERE IS THE KEY HERE IS THE KEY HERE IS THE KEY 
 
 		//calc Jacobian
 		Jacobian[0][2] += -vertices[2*i]*sin(theta) - vertices[2*i+1]*cos(theta);
 		Jacobian[1][2] += vertices[2*i]*cos(theta) - vertices[2*i+1]*sin(theta);
-		//printf("\nvertices[2*i] = %f, vertices[2*i+1] = %f",vertices[2*i],vertices[2*i+1]);
-		//printf("\nJaco[0][2] = %f, Jaco[1][2] = %f",Jacobian[0][2],Jacobian[1][2]);
 
-		//wait();
 		//calc partial gradient in configuration space
 		uCS[0] += uAtt[0] + uRep[0];
 		uCS[1] += uAtt[1] + uRep[1];
 		uCS[2] += uAtt[0]*(Jacobian[0][2]) + uAtt[1]*(Jacobian[1][2]);
 		uCS[2] += uRep[0]*(Jacobian[0][2]) + uRep[1]*(Jacobian[1][2]);
-		//printf("\nuCS[0] = %f\nuCS[1] = %f\nuCS[2] = %f",uCS[0],uCS[1],uCS[2]);
 
 		//reset variables for calculation of partial uCS for next vertex
 		Jacobian[0][2] = 0;
@@ -145,7 +122,7 @@ RigidBodyMove RigidBodyPlanner::ConfigurationMove(void)
 		uRep[0] = 0;
 		uRep[1] = 0;
 	}
-	//printf("REACHED");
+
 	uCS[0] = uCS[0]*scale_cs;
 	uCS[1] = uCS[1]*scale_cs;
 	uCS[2] = uCS[2]*scale_cs*0.15;
@@ -154,8 +131,5 @@ RigidBodyMove RigidBodyPlanner::ConfigurationMove(void)
 	move.m_dy = -uCS[1];
 	move.m_dtheta = -uCS[2];
 
-	printf("\n\t%f\t%f\t%f",move.m_dx,move.m_dy,move.m_dtheta);
-
-	//wait();
 	return move;
 }
